678eaa816e2f641b2e1d9d7ef86193e1eb5da71c
devmarcelomacedo <devmarcelo1eadriana@gmail.com>
2024-10-13 14:09:23 -0300

    mudanca
    mudei o contato

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function registrarOracao() {
     const horario = document.getElementById('horario').value;
     if (nome && horario) {
         const mensagem = `Nome: ${encodeURIComponent(nome)}%0AHorário: ${encodeURIComponent(horario)}`;
-        const link = `https://wa.me/22988302170?text=${mensagem}`;
+        const link = `https://wa.me/21964126015?text=${mensagem}`;
         document.getElementById('link').innerHTML = `<a href="${link}" target="_blank">Enviar pelo WhatsApp</a>`;
     } else {
         alert('Por favor, preencha todos os campos.');
